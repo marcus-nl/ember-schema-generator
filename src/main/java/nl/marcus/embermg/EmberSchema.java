e99f73d512bbad41e1117241062f2c5bd94c3859
Marcus <mgb.klimstra@gmail.com>
2014-09-13 13:05:47 +0200

    JavaDoc WIP.

--- src/main/java/nl/marcus/embermg/EmberSchema.java
+++ src/main/java/nl/marcus/embermg/EmberSchema.java
@@ -4,15 +4,26 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Represents an Ember schema, containing a list of {@link EmberClass}es.
+ * 
+ * @author Marcus Klimstra
+ */
 public class EmberSchema {
 
 	private final List<EmberClass> emberClasses;
 
+	/**
+	 * Creates a new instance with the specified list of {@link EmberClass}es.
+	 */
 	public EmberSchema(List<EmberClass> emberClasses) {
 		super();
 		this.emberClasses = emberClasses;
 	}
 	
+	/**
+	 * The list of {@link EmberClass}es.
+	 */
 	@JsonProperty("classes")
 	public List<EmberClass> getEmberClasses() {
 		return emberClasses;
--- src/main/java/nl/marcus/embermg/EmberTypeRef.java
+++ src/main/java/nl/marcus/embermg/EmberTypeRef.java
@@ -1,17 +1,50 @@
 package nl.marcus.embermg;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Objects;
 
-public class EmberTypeRef {
+/**
+ * A type reference inside an Ember schema.
+ * <p>
+ * For example, a numeric type is represented by an instance of this class with kind "attr" and name "number".
+ * The JSON representation is <code>{ "kind" : "attr", "name" : "boolean" }</code>.
+ * This corresponds to an Ember Data type declaration of "attr('number')". 
+ * <p>
+ * A (non-collection) object type "Foo" is represented by instance of this class with kind "one" and name "Foo".  
+ * By default this corresponds to an Ember Data type declaration of "hasOne('foo')". 
+ * <p>
+ * For collection types the kind is "many". The corresponding Ember Data type declaration would be, for example, "forMany('foo')" by default.
+ * <p>
+ * 
+ * @author Marcus Klimstra
+ */
+public final class EmberTypeRef {
 	
+	/**
+	 * Represents a string attribute type.
+	 */
 	public static final EmberTypeRef STRING  = new EmberTypeRef("attr", "string");
+	
+	/**
+	 * Represents a number attribute type.
+	 */
 	public static final EmberTypeRef NUMBER  = new EmberTypeRef("attr", "number");
+	
+	/**
+	 * Represents a boolean attribute type.
+	 */
 	public static final EmberTypeRef BOOLEAN = new EmberTypeRef("attr", "boolean");
 	
-	public static EmberTypeRef forType(String s) {
-		return new EmberTypeRef("one", s);
+	/**
+	 * Returns an {@link EmberTypeRef} for a non-collection object type with the specified name.
+	 */
+	public static EmberTypeRef forType(String name) {
+		return new EmberTypeRef("one", name);
 	}
-	
+
+	/**
+	 * Returns an {@link EmberTypeRef} for a collection type with the specified name.
+	 */
 	public static EmberTypeRef forCollection(String s) {
 		return new EmberTypeRef("many", s);
 	}
@@ -25,16 +58,47 @@ public class EmberTypeRef {
 		this.name = name;
 	}
 
+	/**
+	 * Returns the type reference kind: 'attr', 'one' or 'many'.
+	 */
 	@JsonProperty
 	public String getKind() {
 		return kind;
 	}
-	
+
+	/**
+	 * Returns the type name.
+	 */
 	@JsonProperty("name")
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(kind, name);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (!(obj instanceof EmberTypeRef)) return false;
+		
+		EmberTypeRef other = (EmberTypeRef) obj;
+		return Objects.equal(this.kind, other.kind)
+			&& Objects.equal(this.name, other.name);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String toString() {
 		return "EmberTypeRef[" + kind + "|" + name + "]";
